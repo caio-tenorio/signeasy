1aaf7be7e54d5396bea217467f7dcb43311173c7
Caio Cavalcanti <cavalcanti971@gmail.com>
2025-08-31 18:12:33 -0300

    Key Cloak and Oauth2

--- adapters/web/src/main/java/com/example/subscriptions/web/dto/PlanDtos.java
+++ adapters/web/src/main/java/com/example/subscriptions/web/dto/PlanDtos.java
@@ -5,7 +5,7 @@ import com.example.subscriptions.domain.common.PlanType;
 import jakarta.validation.constraints.*;
 
 public class PlanDtos {
-    public record CreatePlanRequest(@NotBlank PlanType planType,
+    public record CreatePlanRequest(@NotNull PlanType planType,
                                     @NotBlank String name,
                                     @Min(0) long priceCents,
                                     @NotNull Period period,
